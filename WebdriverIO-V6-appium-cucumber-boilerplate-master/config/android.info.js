d350e62096313caed908c31ef1bb5b034cf13869
anuj <anujmalhotra591@gmail.com>
2024-10-05 16:16:31 +0530

    code done

---
+++ WebdriverIO-V6-appium-cucumber-boilerplate-master/config/android.info.js
@@ -0,0 +1,15 @@
+class AndroidInfo {
+    static deviceName() {
+        return 'Anuj Device'; // pass the udid or devicename
+    }
+
+    static platFormVersion() {
+        return '15'; // pass the platform version
+    }
+
+    static appName() {
+        return 'Celsius Fahrenheit Converter_v1.0.1_apkpure.com.apk'; // pass the apk name
+    }
+}
+
+module.exports = AndroidInfo;
